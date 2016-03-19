d0d14cf06f39efe530ae29e5244fd69d03b4015f
LTTECH <lt222888@gmail.com>
2016-03-19 09:56:28 -0400

    iii

--- TestCodeLoading/src/org/usfirst/frc/team1757/robot/Robot.java
+++ TestCodeLoading/src/org/usfirst/frc/team1757/robot/Robot.java
@@ -4,7 +4,6 @@ package org.usfirst.frc.team1757.robot;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
@@ -16,11 +15,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 
 public class Robot extends IterativeRobot {
-
-	final String defaultAuto = "Default";
-	final String customAuto = "My Auto";
-	String autoSelected;
-	SendableChooser chooser;
 
 	Joystick gamepad;
 	Joystick buttons;
@@ -35,10 +29,6 @@ public class Robot extends IterativeRobot {
 	 * used for any initialization code.
 	 */
 	public void robotInit() {
-		chooser = new SendableChooser();
-		chooser.addDefault("Default Auto", defaultAuto);
-		chooser.addObject("My Auto", customAuto);
-		SmartDashboard.putData("Auto choices", chooser);
 
 		gamepad = new Joystick(0);
 		buttons = new Joystick(1);
@@ -57,24 +47,14 @@ public class Robot extends IterativeRobot {
 	 * If using the SendableChooser make sure to add them to the chooser code above as well.
 	 */
 	public void autonomousInit() {
-		autoSelected = (String) chooser.getSelected();
-		//		autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
-		System.out.println("Auto selected: " + autoSelected);
+		
 	}
 
 	/**
 	 * This function is called periodically during autonomous
 	 */
 	public void autonomousPeriodic() {
-		switch(autoSelected) {
-		case customAuto:
-			//Put custom auto code here   
-			break;
-		case defaultAuto:
-		default:
-			//Put default auto code here
-			break;
-		}
+
 	}
 
 	/**
