bc83b5ddd1db7afb4a564d22b6becc07868369aa
LTTECH <LTTECH@users.noreply.github.com>
2016-03-19 21:40:58 -0400

    Update MotorRunnable.java

--- TestCodeLoading/src/org/usfirst/frc/team1757/robot/MotorRunnable.java
+++ TestCodeLoading/src/org/usfirst/frc/team1757/robot/MotorRunnable.java
@@ -13,14 +13,16 @@ public class MotorRunnable implements Runnable {
 	public MotorRunnable(CANTalon talon, double motorSpeed) {
 		this.talon4 = talon;
 		this.motorSpeed = motorSpeed;
-		thread = new Thread();
-		thread.start();
 	}
 	
 	@Override
 	public synchronized void run() {
 		// TODO Auto-generated method stub
 		//talon4.set(motorSpeed);
+
+
+      thread = new Thread();
+		thread.start();
 		System.out.println("Pressed button. motorSpeed: " + motorSpeed);
 		System.out.println("Sleeping for 2500 ms.");
 		Timer.delay(2);
